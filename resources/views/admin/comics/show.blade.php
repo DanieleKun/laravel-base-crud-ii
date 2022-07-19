d1c4fefb4673f571f8974a0771eb55dec0113316
DanieleKun <pucciodaniele95@gmail.com>
2022-07-19 16:57:06 +0200

    first commit

---
+++ resources/views/admin/comics/show.blade.php
@@ -0,0 +1,24 @@
+@extends('admin.templates.base')
+
+@section('mainContent')
+
+    <h1>Show Comics</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Campo:</th>
+                <th>Valore</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comic->toArray() as $key => $value)
+                <tr>
+                    <td>{{ $key }}</td>
+                    <td>{{ $value }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+@endsection
---
+++ resources/views/front/templates/home.blade.php
@@ -0,0 +1,8 @@
+@extends('front.templates.base')
+@include('admin.partials.header')
+
+@section('mainContent')
+    <main>
+        <h1>Home Page</h1>
+    </main>
+@endsection
